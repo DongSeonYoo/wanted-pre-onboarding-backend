269761fb229d834e312fdbb110fa069f932b313c
ehdtjs0612 <inko51366@naver.com>
2023-10-16 19:21:28 +0900

    Feat: add modify notice api

--- routes/recruit-notice.js
+++ routes/recruit-notice.js
@@ -49,4 +49,42 @@ router.post("/", async (req, res, next) => {
     res.send(result);
 });
 
+// 채용 공고 수정 api
+router.put("/", async (req, res, next) => {
+    const { noticeId, position, reward, content, skills } = req.body;
+    const result = {
+        message: "",
+        data: {}
+    };
+
+    try {
+        validator(noticeId, "noticeId").checkInput().isNumber();
+        validator(position, "position").checkInput().checkLength(1, RECRUIT_NOTICE.MAX_POSITION_LENGTH);
+        validator(reward, "reward").checkInput().isNumber();
+        validator(content, "content").checkInput().checkLength(1, RECRUIT_NOTICE.MAX_CONTENT_LENGTH);
+        validator(skills, "skills").checkInput().checkLength(1, RECRUIT_NOTICE.MAX_SKILLS_LENGTH);
+
+        // 나중에 추가 될 권한 체크 후
+        const modifyNoticeSql = `UPDATE 
+                                        recruit_notice_tb
+                                    SET
+                                        position = $1,
+                                        reward = $2,
+                                        content = $3,
+                                        skills = $4
+                                    WHERE
+                                        id = $5`;
+        const modifyNoticeParam = [position, reward, content, skills, noticeId];
+        const modifyNoticeResult = await pool.query(modifyNoticeSql, modifyNoticeParam);
+        if (modifyNoticeResult.rowCount === 0) {
+            throw new BadRequestException("해당하는 공고가 존재하지 않습니다.");
+        }
+        result.message = "채용 공고 수정 성공";
+
+    } catch (error) {
+        return next(error);
+    }
+    res.send(result);
+});
+
 module.exports = router;
