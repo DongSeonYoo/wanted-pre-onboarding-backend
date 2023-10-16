7ff9247921c8ef6426fe6f4ff14334d8b69dc10d
ehdtjs0612 <inko51366@naver.com>
2023-10-16 22:23:45 +0900

    Feat: add global variable other notice per page

--- modules/global.js
+++ modules/global.js
@@ -3,6 +3,7 @@ module.exports = {
         MAX_POSITION_LENGTH: 100,
         MAX_CONTENT_LENGTH: 600, // 협의 후 변경 가능
         MAX_SKILLS_LENGTH: 100,
-        MAX_NOTICES_PER_PAGE: 10 // 한 페이지 당 10개의 공고를 보여줌
+        MAX_NOTICES_PER_PAGE: 10, // 한 페이지 당 10개의 공고를 보여줌
+        MAX_OTHER_NOTICE_PER_PAGE: 5 // 회사가 올린 다른 채용공고를 5개씩 보여줌
     },
 }
