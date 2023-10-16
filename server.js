345c27ce6acd3deb5b6aa0164c186aba297d66e8
ehdtjs0612 <inko51366@naver.com>
2023-10-16 22:26:12 +0900

    Feat: add apply routes and connect in server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const errorHandling = require("./middleware/errorHandling");
 const recruitNoticeApi = require("./routes/recruit-notice");
+const applyApi = require("./routes/apply");
 
 // global middleware
 app.use(express.json());
@@ -11,6 +12,7 @@ app.use(cookieParser());
 
 // api call middleware
 app.use("/recruit-notice", recruitNoticeApi);
+app.use("/apply", applyApi);
 
 // error handling middleware
 app.use(errorHandling());
