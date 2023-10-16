d7579d5c1418b5247a3a080707fc1b9dec7972dd
ehdtjs0612 <inko51366@naver.com>
2023-10-16 18:40:28 +0900

    Feat: import error handling middleware, cookie parser package module

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+const cookieParser = require("cookie-parser");
+const errorHandling = require("./middleware/errorHandling");
 const recruitNoticeApi = require("./routes/recruit-notice");
-
 
 // global middleware
 app.use(express.json());
