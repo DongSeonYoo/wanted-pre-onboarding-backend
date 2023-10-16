057013c4d211e650dea121993a33ee6f17d87648
ehdtjs0612 <inko51366@naver.com>
2023-10-16 18:19:00 +0900

    Feat: connect routes recruit-notice in server.js

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const recruitNoticeApi = require("./routes/recruit-notice");
+
 
 // global middleware
 app.use(express.json());
 app.use(cookieParser());
 
 // api call middleware
+app.use("/recruit-notice", recruitNoticeApi);
 
 // error handling middleware
-
+app.use(errorHandling());
 module.exports = app;
