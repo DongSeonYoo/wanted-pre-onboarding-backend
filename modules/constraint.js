7192028c8b125a41ccc1fe762562b813d2523a26
ehdtjs0612 <inko51366@naver.com>
2023-10-17 14:13:01 +0900

    Comment: Add Constraint Description

--- modules/constraint.js
+++ modules/constraint.js
@@ -1,4 +1,7 @@
 module.exports = {
+    // unique 이메일 제약조건
     UNIQUE_EMAIL_TO_USER_TB: "unique_email_to_user_tb",
+
+    // 채용 공고 등록 시 fk 제약조건
     FK_COMPANY_TO_RECRUIT_TB: "fk_company_tb_to_recruit_notice_tb",
 }
