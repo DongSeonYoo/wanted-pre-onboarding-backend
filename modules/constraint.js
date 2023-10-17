9819499c54abe1cf7457be51f9390800232522bd
ehdtjs0612 <inko51366@naver.com>
2023-10-17 14:13:35 +0900

    Feat: add fk constraint for apply to recruit-notice

--- modules/constraint.js
+++ modules/constraint.js
@@ -4,4 +4,8 @@ module.exports = {
 
     // 채용 공고 등록 시 fk 제약조건
     FK_COMPANY_TO_RECRUIT_TB: "fk_company_tb_to_recruit_notice_tb",
+
+    // 지원 요청 시 fk 제약조건
+    FK_RECRUIT_NOTICE_TO_APPLY_TB: "fk_recruit_notice_tb_to_apply_tb",
+    FK_USER_TO_APPLY_TB: "fk_user_tb_to_apply_tb"
 }
