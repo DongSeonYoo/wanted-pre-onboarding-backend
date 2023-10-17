7ab698d3e33b23d403f1064cebd5f2be11f45518
ehdtjs0612 <inko51366@naver.com>
2023-10-17 13:56:41 +0900

    Feat: add password, name, email regex in global variable

--- modules/global.js
+++ modules/global.js
@@ -1,4 +1,9 @@
 module.exports = {
+    AUTH: {
+        MAX_EMAIL_LENGTH: 256,
+        MAX_PASSWORD_LENGTH: 32
+    },
+
     RECRUIT_NOTICE: {
         MAX_POSITION_LENGTH: 100,
         MAX_CONTENT_LENGTH: 600, // 협의 후 변경 가능
@@ -6,4 +11,10 @@ module.exports = {
         MAX_NOTICES_PER_PAGE: 10, // 한 페이지 당 10개의 공고를 보여줌
         MAX_OTHER_NOTICE_PER_PAGE: 5 // 회사가 올린 다른 채용공고를 5개씩 보여줌
     },
+
+    REGEX: {
+        PASSWORD: /^(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&+=!.]).{8,}$/, // 최소 8자 특수문자 포함
+        NAME: /^[가-힣a-zA-Z ]{2,16}$/, // 한글 + 영어 2 ~ 16자
+        EMAIL: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ // 이메일 정규식
+    }
 }
